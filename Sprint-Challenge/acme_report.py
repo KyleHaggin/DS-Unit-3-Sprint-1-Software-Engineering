81de028fb04a22e8d5d079dca43170fbf3964456
KyleHaggin <KyleHaggin@users.noreply.github.com>
2019-10-04 09:51:39 -0700

    Added code to acme report. Updated acme_report and acme_test to conform to pep8 standards

--- Sprint-Challenge/acme_report.py
+++ Sprint-Challenge/acme_report.py
@@ -8,7 +8,11 @@ NOUNS = ['Anvil', 'Catapult', 'Disguise', 'Mousetrap', '???']
 
 def generate_products(num_products=30):
     products = []
-    products = 
+    for x in range(num_products):
+        hldr_name = ADJECTIVES[randint(0, 4)] + ' ' + NOUNS[randint(0, 5)]
+        hldr = Product(hldr_name, randint(5, 100), randint(5, 100),
+                       flammability=uniform(0, 2.5))
+        products.extend(hldr)
     return products
 
 
