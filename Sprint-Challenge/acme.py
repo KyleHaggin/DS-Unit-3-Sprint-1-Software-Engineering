eb94c0b4c2d6a74f926d5d777d5cb96024d9342c
KyleHaggin <KyleHaggin@users.noreply.github.com>
2019-10-04 08:34:54 -0700

    Add acme.py and add objects and functions to acme.py

---
+++ Sprint-Challenge/acme.py
@@ -0,0 +1,34 @@
+import unittest
+import random
+
+
+'''
+Sprint challenge Part 1
+'''
+
+
+class Product:
+    def __init__(self, name, price=10, weight=20, flammability=0.5,
+                 identifier=random.randint(1000000, 9999999)):
+        self.name = name
+        self.price = price
+        self.weight = weight
+        self.flammability = flammability
+        self.identifier = identifier
+
+    def stealability(self):
+        if (self.price / self.weight) < 0:
+            return 'Not so stealable'
+        elif (self.price / self.weight) < 1:
+            return 'Kinda stealable'
+        else:
+            return 'Very Stealable'
+
+    def explode(self):
+        explode_index = self.flammability * self.weight
+        if explode_index < 10:
+            return '...fizzle.'
+        elif explode_index < 50:
+            return '...boom!'
+        else:
+            return '...BABOOM!'
